567cbc2addbdc4f8f084ab38894a11afa9627928
Christabel-Akpene <dc.akpene@gmail.com>
2025-11-26 12:56:13 +0000

    calculate price & days

--- components/property/BookingSection.tsx
+++ components/property/BookingSection.tsx
@@ -1,23 +1,64 @@
+import { useState } from "react";
+import { Separator } from "../ui/separator";
+
 const BookingSection: React.FC<{ price: number }> = ({ price }) => {
+  
+  const [checkIn, setCheckIn] = useState("");
+  const [checkout, setCheckOut] = useState("");
+
+  const bookedDays = (checkIn:string, checkout:string) => {
+      if (checkIn && checkout){
+        const check_out = new Date(checkout);
+        const check_in = new Date(checkIn);
+
+        const diffDays = (check_out.getTime() - check_in.getTime())/(1000 * 60 * 60 * 24);        
+        return diffDays
+      }
+      return 0;
+  }
+
+  const days = bookedDays(checkIn, checkout); 
+  
+
   return (
     <div className="bg-white p-6 shadow-md rounded-lg">
       <h3 className="text-xl font-semibold">${price}/night</h3>
       <div className="mt-4">
-        <label>Check-in</label>
-        <input type="date" className="border p-2 w-full mt-2" />
+        <label className="semibold">Check-in</label>
+        <input onChange={(e)=> setCheckIn(e.target.value)} value={checkIn}  type="date"  className="border p-2 w-full mt-2" />
       </div>
       <div className="mt-4">
-        <label>Check-out</label>
-        <input type="date" className="border p-2 w-full mt-2" />
+        <label className="semibold">Check-out</label>
+        <input onChange={(e)=> setCheckOut(e.target.value)} value={checkout} type="date" className="border p-2 w-full mt-2" />
       </div>
 
-      {/* Total payment */}
-      <div className="mt-4">
-        <p>Total payment: <strong>${price * 7}</strong></p>
-      </div>
+      {
+        days > 0 && (
+          <div className="my-4 flex flex-col space-y-2">
+            <div className="flex justify-between">
+              <p className="text-ring text-sm">${price} * {days} nights</p>
+              <p className="font-semibold">${(price * days)}</p>
+            </div>
+            <div className="flex justify-between">
+              <p  className="text-ring text-sm ">Weekly discounts</p>
+              <p className="font-semibold">-$88</p>
+            </div>
+            <div className="flex justify-between mb-4">
+              <p  className="text-ring text-sm">Service fee</p>
+              <p className="font-semibold">${33}</p>
+            </div>
+            <Separator/>
+            <div className="flex justify-between">
+              <p  className="text-ring text-sm">Total Payment</p>
+              <p className="text-green-700 font-semibold">${(price * days) - 88 + 33}</p>
+            </div>
+          </div>
+        )
+      }
+
 
       {/* Reserve button */}
-      <button className="mt-4 bg-green-500 text-white py-2 px-4 rounded-md">Reserve now</button>
+      <button className="mt-4 bg-green-700 text-white py-2 px-4 rounded-md w-full">Reserve now</button>
     </div>
   );
 };
